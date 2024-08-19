dc55fe37a248409b0963cba4741e8d67f5b2b727
yvanddniyo <nyvan80076@gmail.com>
2024-08-19 10:31:03 +0200

    working on finish touch on todo app on profile display

--- app/todos/create/page.tsx
+++ app/todos/create/page.tsx
@@ -1,25 +1,21 @@
-'use server'
+"use server";
 import { Dark } from "@/app/ui/darkMode/darkMode";
-import { addTodo, getData } from "@/lib/actions/todoActions";
 import AddTodo from "@/lib/components/addTodo";
 import Todo from "@/lib/components/todo";
-import 'react-toastify/dist/ReactToastify.css';
-import { todoType } from "@/lib/types/todoTypes";
+import "react-toastify/dist/ReactToastify.css";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export default async function page (){
-  const session:any = await auth()
+export default async function page() {
+  const session: any = await auth();
   if (!session?.user) {
-    redirect("/api/auth/signin")
-  }  
- return(
-   <main className="w-full md:px-12 py-12 px-3">
-      <Dark 
-       session = {session?.user}
-      />
+    redirect("/api/auth/signin");
+  }
+  return (
+    <main className="w-full md:px-12 py-12 px-3">
+      <Dark session={session?.user} />
       <AddTodo />
-      <Todo /> 
-   </main>
- )
+      <Todo />
+    </main>
+  );
 }
