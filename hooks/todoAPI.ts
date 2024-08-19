f854e8422c84b561f6f427edd64b6df6deeb6e51
yvanddniyo <nyvan80076@gmail.com>
2024-08-19 12:18:49 +0200

    fixing on the axios the error

--- hooks/todoAPI.ts
+++ hooks/todoAPI.ts
@@ -1,27 +1,27 @@
 import { TodoType } from "@/lib/types/todoTypes";
 import axios from "axios"
-
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 export const fetchTodos = async () => {
-    const { data } = await axios.get('/api/todos');
+    const { data } = await axios.get(`${BASE_URL}/api/todos`);
     return data;
   };
 
 export const  createTodos = async(dataForm: TodoType) => {
-    const { data } = await axios.post('/api/todos', dataForm)
+    const { data } = await axios.post(`${BASE_URL}/api/todos`, dataForm)
     return data;
 }
 
 export const deleteTodos = async(id: number) => {
-    const todo = await axios.delete(`/api/todos/${id}`)
+    const todo = await axios.delete(`${BASE_URL}/api/todos/${id}`)
     return todo;
 }
 
 export const updateTodos = async (id: number, updatedData: { title?: string; description?: string }) => {
-    const response = await axios.patch(`/api/todos/${id}`, updatedData);
+    const response = await axios.patch(`${BASE_URL}/api/todos/${id}`, updatedData);
     return response.data;
 };
 
 export const toggleTodos = async (id: number, toggle:{done: boolean}) => {
-    const response = await axios.put(`/api/todos/${id}`, toggle);
+    const response = await axios.put(`${BASE_URL}/api/todos/${id}`, toggle);
     return response;
 }
