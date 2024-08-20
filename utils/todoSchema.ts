d71670a5a1c2e5c36f4813da40e98c0ec11f69b1
yvanddniyo <nyvan80076@gmail.com>
2024-08-20 11:19:46 +0200

    fixing validation on adding a task

--- utils/todoSchema.ts
+++ utils/todoSchema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const todoSchema = z.object({
-  title: z.string().min(1, "Title is required")
+  title: z.string().min(4, "Title must have 4 characters above")
   .max(10, "Title must not exceed 10 characters"),
-  description: z.string().min(1, "Description is required")
+  description: z.string().min(10, "Description must have 4 characters above")
 });
 
 export type TodoSchema = z.infer<typeof todoSchema>;
